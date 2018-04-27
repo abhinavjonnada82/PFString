2ec1bfd3029d9cb9ee1eff7c37111c76411be57b
b956d578 <b956d578@cslab-node-9.cs.wichita.edu>
2018-04-27 18:48:04 -0500

    Test Functions Fixed

--- src/PFAString.cpp
+++ src/PFAString.cpp
@@ -1,183 +1,148 @@
+/*
+ * Abhinav Jonnada
+ * b956d578
+ * 1/31/2018
+ * Program 5
+ *
+ */
+
+
+
+
+
+
 #include <iostream>
-#include "PFAString.hpp"
 #include <cstdlib>
+#include <stdexcept>
+#include "PFAString.hpp"
 
 class Trivial {
  public:
    Trivial() {};
 };
+
 PFAString::PFAString()
 {
-    arr = new std::string[100];
-    caPacity = 100;              // caPacity and size are two different things
+    caPacity = 0;
     siZe = 0;
-
+    arr = nullptr;
 }
-
 PFAString::PFAString(int cap, std::string entered)
 {
-    arr = new std::string[cap];
-    if (cap < 0)
+    arr = new std::string[100];
+    if(cap < 0)
     {
         caPacity = 0;
     }
     else
     {
-      caPacity = cap;
-      siZe = cap;
-      for (int i =0; i < caPacity; i++)
-      {
-          arr[i] = entered;
-      }
+        caPacity = cap;
+        siZe = cap;
+        for(int i = 0; i < caPacity; i++)
+        {
+            arr[i] = entered;
+        }
     }
-
 }
-
-PFAString::PFAString(const PFAString& PFAobj)
+PFAString::PFAString(const PFAString& PFAobj)   //copy constructor
 {
-    arr = new std::string[100];
+    arr = new std::string[PFAobj.siZe];
     caPacity = PFAobj.siZe;
     siZe = PFAobj.siZe;
 
-    for(int i = 0; i < PFAobj.siZe; i++)
+    for(int x = 0; x < PFAobj.siZe; x++)
     {
-        arr[i] = PFAobj.arr[i];
-
+        arr[x] = PFAobj.arr[x];
     }
 }
-
-PFAString::PFAString(PFAString&& PFAobj)
-
-    // :arr(PFAobj.arr)
-    // ,caPacity(PFAobj.caPacity)
-    // ,siZe(PFAobj.siZe)
-  {
-    siZe = PFAobj.siZe;
-    arr = PFAobj.arr;
-    caPacity = PFAobj.caPacity;
-    PFAobj.arr = nullptr;
-  }
-
-
+PFAString::PFAString(PFAString&& PFAobj)    //move constructor
+    :arr(PFAobj.arr)
+    ,caPacity(PFAobj.caPacity)
+    ,siZe(PFAobj.siZe)
+    {
+        PFAobj.arr = nullptr;
+    }
 PFAString::~PFAString()
 {
-    if(arr != NULL)
+    if(arr != nullptr)
     {
         delete []arr;
-        arr = NULL;
+        arr = nullptr;
     }
     else
-        std::cout << "pointer is NULL....";
-}
+    {
+        arr = nullptr;
+    }
 
+}
 const int PFAString::capacity() const
 {
-    return caPacity;
+  return caPacity;
 }
-
 const int PFAString::size() const
 {
     return siZe;
 }
-
 void PFAString::push_back(std::string entered)
 {
-	siZe += 1;
-	
-	if ( caPacity == 0) {
-		arr = new std::string[5];
-		caPacity = 5;
-	}
-	if ( siZe > caPacity) {
-		caPacity *= 2;
-		std:: string* temp = new std::string[caPacity];
-		for (int i = 0 ; i < siZe - 1; i++) {
-			temp[i] = arr[i];
-		}
-		delete[] arr;
-		arr = temp;
-	}
-	arr[(siZe -1)] = entered;
+    if(arr == nullptr)
+    {
+        arr = new std::string[caPacity+1];
+        caPacity += 1;
+    }else if(siZe == caPacity)
+    {
+         std::string *arr_temp = new std::string[caPacity*2];
+         for(int i = 0; i < caPacity; i++)
+         {
+             arr_temp[i] = arr[i];
+         }
+         caPacity = caPacity * 2;
+         delete []arr;
+         arr = arr_temp;
+    }
+    siZe += 1;
+    arr[siZe - 1] = entered;
 }
-		
-		
-  /*if(siZe == caPacity)}
-   * 
-  {
-    caPacity = caPacity * 2;
-  }
-  siZe += 1;
-  arr[siZe - 1] = entered;
-  * */
-    // if(arr == nullptr)
-    // {
-    //   arr = new std::string[caPacity + 1];
-    //   caPacity += 1;
-    // }
-    // else if(siZe = caPacity)
-    // {
-    //   std::string *arr_tm = new std::string[caPacity * 2];
-    //   for (int i = 0;  i < caPacity; i++)
-    //   {
-    //     arr_tm[i] = arr[i];
-    //   }
-    //   caPacity = caPacity * 2;
-    //   delete []arr;
-    //   arr = arr_tm;
-    // }
-    // siZe += 1;
-    // arr[siZe -1] = entered;
-
-
 void PFAString::pop_back()
-{ 
-	if (siZe != 0) {
-        arr[siZe-1].clear(); //erases contents of string
+{
+    if(siZe != 0)
+    {
+        arr[siZe - 1].clear();      //erases contents of string
         siZe -= 1;
-  }
-  else {
+
+    }
+    else {
 	  throw std::out_of_range ("Popping an empty array");
  }
 }
- void PFAString::resize(int des_size, std::string destin)
- {
-     if(des_size >= 0)
-     {
-         if(des_size > siZe)
-         {
-			  delete[] arr;
-             arr = new std::string[des_size];
-             
-             for(int x = 0; x < des_size; x++)
-             {
-                 arr[x] = destin;
-                 
-             }
-           
-             siZe = des_size;
-             caPacity = des_size;
-         }
-         else if (des_size < siZe)
-         {
-             siZe = des_size; //if out of scope strings will be deleted
-             for(int x = 0; x < des_size; x++)
-             {
-                 arr[x] = destin;
-                 
-             }
-         }
-     }
-     else
+void PFAString::resize(int des_size, std::string destin)
+{
+    if(des_size >= 0)
+    {
+        if(des_size > siZe)
+        {
+            for(int x = siZe; x < des_size; x++)
+            {
+                arr[x] = destin;
+            }
+            siZe = des_size;
+            caPacity = des_size;
+        }
+        else if(des_size < siZe)
+        {
+            siZe = des_size;   //if out of scope strings will be deleted
+        }
+    }
+    else
         siZe = 0;
- }
-
+}
 void PFAString::empty_array()
 {
-	if (siZe == 0) {
+    if (siZe == 0) {
     throw Trivial();
-}}
-
-std::string& PFAString::operator [] (const int index)
+}
+}
+std::string& PFAString::operator [](const int index)
 {
     if(index < siZe && index >= 0)
     {
@@ -185,21 +150,19 @@ std::string& PFAString::operator [] (const int index)
     }
     else
     {
-        std::cout << std::endl << "Invalid Index" << std::endl;
+        std::cout << std::endl << "Invalid index" << std::endl;
         std::exit(1);
     }
-
-} 
-       // Needs to work as l-value
+}
 std::string& PFAString:: at(int i) {
 	if (i < 0 || i >= siZe) {
-		throw std::out_of_range("Its beyond range"); 
+		throw std::out_of_range("Its beyond range");
 	}
 	else {
 		return arr[i];
 	}
 }
-
+//overload copy operator =
 void PFAString::operator = (const PFAString& right)
 {
     arr = new std::string[100];
@@ -211,7 +174,7 @@ void PFAString::operator = (const PFAString& right)
         arr[i] = right.arr[i];
 
     }
-} //overload copy operator =
+}
 PFAString& PFAString::operator = (PFAString&& right)
 {
     this -> siZe = right.size();
@@ -219,7 +182,7 @@ PFAString& PFAString::operator = (PFAString&& right)
     this -> arr = right.arr;
     right.arr = nullptr;
     return *this;
-}//overload move operator =
+}
 
 bool operator ==(const PFAString& left, const PFAString& right)
 {
@@ -256,26 +219,17 @@ bool operator ==(const PFAString& left, const PFAString& right)
     return meter;
 
 }
-    /* if (!(left < right) && !(right < left))
-     {
-         return true;
-     }
-     else
-     {
-         return false;
-     }
-
-}*/
-
 bool operator !=(const PFAString& left, const PFAString& right)
 {
-
-   if (left == right)
-    return false;
-   else
-    return true;
+    if (left == right)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
-
 bool operator <(const PFAString& left, const PFAString& right)
 {
     int size_up;
@@ -291,12 +245,12 @@ bool operator <(const PFAString& left, const PFAString& right)
         if(left.arr[i] == right.arr[i])
         {
             meter = false;
-            cur = false;
+            cur = true;
             break;
         }
         else if(left.arr[i] > right.arr[i])
         {
-            meter = true;
+            meter = false;
             cur = false;
             break;
         }
@@ -313,7 +267,7 @@ bool operator <(const PFAString& left, const PFAString& right)
         {
             meter = false;
         }
-        else
+        else if(left.siZe < right.siZe)
         {
             meter = true;
         }
@@ -340,55 +294,23 @@ bool operator <=(const PFAString& left, const PFAString& right)
     {
         return true;
     }
-    else if (left == right)
+    else if(left == right)
         return true;
     else
         return false;
 }
-
-bool operator > (const PFAString& left, const PFAString& right)
+bool operator >(const PFAString& left, const PFAString& right)
 {
-  int size_up;
-  bool meter;
-  bool cur = false;
-
-  if (left.siZe < right.siZe)
-      size_up = left.siZe;
-  else
-      size_up = right.siZe;
-  for (int i = 0; i< size_up; i++)
-  {
-      if(left.arr[i] == right.arr[i])
-      {
-          meter = false;
-          cur = false;
-          break;
-      }
-      else if(left.arr[i] < right.arr[i])
-      {
-          meter = true;
-          cur = false;
-          break;
-      }
-      else
-      {
-          meter = true;
-          cur = false;
-          break;
-      }
-  }
-  if(meter == false && cur == true)
-  {
-      if(right.siZe >= left.siZe)
-      {
-          meter = false;
-      }
-      else
-      {
-          meter = true;
-      }
-  }
-  return meter;
+    if((left < right) == false)
+    {
+        if((left == right) == false)
+            return true;
+        else
+            return false;
+    }
+    else
+        return false;
+}
 
     // if((left < right) == false)
     // {
@@ -403,11 +325,10 @@ bool operator > (const PFAString& left, const PFAString& right)
     // }
     // else
     //     return false;
-}
 
 bool operator >= (const PFAString& left, const PFAString& right)
 {
-  if((left > right) == true)
+  if((left < right) == false)
   {
       return true;
   }
